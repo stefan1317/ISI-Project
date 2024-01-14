2d895779a221d3c282238021765f55e882d3308a
Cosmin <corneacosmin01@yahoo.com>
2024-01-14 16:21:26 +0200

    Functional SearchBar

--- demo/lab5/src/app/pages/esri-map/esri-map.component.ts
+++ demo/lab5/src/app/pages/esri-map/esri-map.component.ts
@@ -35,10 +35,12 @@ import Point from '@arcgis/core/geometry/Point';
 
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 
+import { AppSearchBarComponent } from "./search-bar.component";
+
 @Component({
   selector: "app-esri-map",
   templateUrl: "./esri-map.component.html",
-  styleUrls: ["./esri-map.component.scss"]
+  styleUrls: ["./esri-map.component.scss"],
 })
 export class EsriMapComponent implements OnInit, OnDestroy {
   // The <div> where we will place the map
@@ -49,6 +51,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   view: esri.MapView;
   pointGraphic: esri.Graphic;
   graphicsLayer: esri.GraphicsLayer;
+  searchBar: AppSearchBarComponent;
 
   // Attributes
   zoom = 10;
@@ -66,6 +69,7 @@ export class EsriMapComponent implements OnInit, OnDestroy {
   subscriptionObj: Subscription;
 
   onSearch(countryName: string) {
+    console.log(countryName);
   }
   constructor(
     private fbs: FirebaseService
